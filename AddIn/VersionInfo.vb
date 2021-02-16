39c411ec14331f261b6d01a504eb041c9bea0502
SteveBeadle <44027870+SteveBeadle@users.noreply.github.com>
2021-02-16 16:52:10 +0000

    Cleanup

--- AddIn/VersionInfo.vb
+++ AddIn/VersionInfo.vb
@@ -3,10 +3,10 @@ Imports System.Reflection
 Imports System.Runtime.CompilerServices
 
 
-<Assembly: AssemblyVersion("6.83.7417.1")>
+<Assembly: AssemblyVersion("0.0.0.1")>
 <Assembly: AssemblyCompany("Schneider Electric")>
 <Assembly: AssemblyCopyright("See License in Source Code")>
 <Assembly: AssemblyProduct("Geo SCADA AddIn Tools")>
-<Assembly: AssemblySupportLink("")>
-<Assembly: AssemblyHelpLink("")>
-<Assembly: AssemblyUpdateInformation("")>
+<Assembly: AssemblySupportLink("https://community.exchange.se.com/t5/Geo-SCADA-Expert-Forum/bd-p/ecostruxure-geo-scada-expert-forum")>
+<Assembly: AssemblyHelpLink("https://community.exchange.se.com/t5/Geo-SCADA-Expert-Forum/bd-p/ecostruxure-geo-scada-expert-forum")>
+<Assembly: AssemblyUpdateInformation("https://community.exchange.se.com/t5/Geo-SCADA-Expert-Forum/bd-p/ecostruxure-geo-scada-expert-forum")>
--- DriverAddIn/VersionInfo.cs
+++ DriverAddIn/VersionInfo.cs
@@ -3,11 +3,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 
-[assembly: AssemblyVersion("6.83.7417.1")]
+[assembly: AssemblyVersion("0.0.0.1")]
 [assembly: AssemblyCompany("Schneider Electric")]
 [assembly: AssemblyCopyright("See License in Source Code")]
 [assembly: AssemblyProduct("Geo SCADA AddIn Tools")]
-[assembly: AssemblySupportLink("")]
-[assembly: AssemblyHelpLink("")]
-[assembly: AssemblyUpdateInformation("")]
+[assembly: AssemblySupportLink("https://community.exchange.se.com/t5/Geo-SCADA-Expert-Forum/bd-p/ecostruxure-geo-scada-expert-forum")]
+[assembly: AssemblyHelpLink("https://community.exchange.se.com/t5/Geo-SCADA-Expert-Forum/bd-p/ecostruxure-geo-scada-expert-forum")]
+[assembly: AssemblyUpdateInformation("https://community.exchange.se.com/t5/Geo-SCADA-Expert-Forum/bd-p/ecostruxure-geo-scada-expert-forum")]
 
